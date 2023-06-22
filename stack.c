51482287aafe41cc76dc418620d44112e3315f80
Brian Omondi <omondiibrian00@gmail.com>
2023-06-22 22:22:54 +0300

    adding stack

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ *f_stack - top of the stack
+ *@head: stack head
+ *@count: line number
+ *Return: Void
+ */
+void f_stack(stack_t **head, unsigned int count)
+{
+	(void)head;
+	(void)count;
+	com.lineno = 0;
+}
